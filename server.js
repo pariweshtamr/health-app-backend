2f654c083ea32b0b18b43f2fb9531bf6827369aa
pariwesh <tamrpariwesh@gmail.com>
2023-10-30 22:19:57 +1100

    initial commit, added schemas, initialized server, added mongodb connection

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import mongoClient from "./lib/db.js"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8000
+
+const corsOptions = {
+  origin: true,
+}
+
+// Connect db
+mongoClient()
+
+// middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+
+app.get("/", (req, res) => {
+  res.send("You have reached health app server!")
+})
+
+// Global error handler
+app.use((error, req, res, next) => {
+  const errorStatus = error.status || 500
+  const errorMsg = error.message || "Something went wrong!"
+
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMsg,
+    stack: error.stack,
+  })
+})
+
+app.listen(port, (error) => {
+  error ? console.log(error) : console.log(`Server is running on port ${port}`)
+})
